b06b16f2754b5839c8868a604843a4119dd14342
Amit <amit@AZ-iMac.local>
2018-09-26 22:12:11 -0400

    added actions and reducers folder to src

--- src/index.js
+++ src/index.js
@@ -4,5 +4,9 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
